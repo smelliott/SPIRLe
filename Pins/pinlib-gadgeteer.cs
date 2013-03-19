5fbede62215382a51b0461de0b2673c749452556
Elliott <elliott@ENET-HADRON.(none)>
2013-03-19 15:32:35 +0000

    testing something

--- Pins/pinlib-gadgeteer.cs
+++ Pins/pinlib-gadgeteer.cs
@@ -4,8 +4,8 @@ using Microsoft.SPOT.Hardware;
 using Gadgeteer;
 
 namespace SPIRLe {
-	public class Pin {
-		public static bool set_pin_digital(int socket, int pin, bool v) {
+	public class DigitalPin {
+		public static bool setv(int socket, int pin, bool v) {
 			Cpu.Pin p;
 			try {
 				p = Socket.GetSocket(socket, true, null, null).CpuPins[pin];
@@ -17,19 +17,26 @@ namespace SPIRLe {
 			return true;
 		}
 
-		public static void set_pin_analog(int socket, int pin, int v) {
-			AnalogOutput ao = new AnalogOutput((Cpu.AnalogOutputChannel)pin);
-			ao.WriteRaw(v);
-		}
-
-		public static bool get_pin_digital(int socket, int pin) {
+		public static bool getv(int socket, int pin) {
 			InputPort ip = new InputPort((Cpu.Pin)pin, false, Port.ResistorMode.Disabled); //need to check this is right
 			return ip.Read();
 		}
 
-		public static int get_pin_analog(int socket, int pin) {
+	}
+
+	public class AnalogPin {
+		public static void setv(int socket, int pin, int v) {
+			AnalogOutput ao = new AnalogOutput((Cpu.AnalogOutputChannel)pin);
+			ao.WriteRaw(v);
+		}
+
+		public static int getv(int socket, int pin) {
 			AnalogInput ai = new AnalogInput((Cpu.AnalogChannel)pin);
 			return ai.ReadRaw();
 		}
+	}
+
+	public class PwmPin {
+		public static void setv()
 	}
 }
